3d5cd366bcd6538dbf945fb4cb883d161bd48855
Alok Kumar <alok345205@gmail.com>
2026-09-17 21:28:06 +0530

    Prepare Flask backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -1940,8 +1940,8 @@ def get_history(user_id):
         }), 500
 if __name__ == "__main__":
 
-    app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
-    )
+     app.run(
+    debug=True,
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", 5000))
+)
